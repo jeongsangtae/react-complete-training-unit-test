1e984ee7567a56d1f53f7b04eca9907dd5a8a4e5
jeongsangtae <wjdtkdxo55@naver.com>
2023-11-22 06:25:09 +0900

    React Complete Training Unit Test - Write your first test (App.js edit, Greeting.js, Greeting.test.js add)

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+test("renders Hello World as a text", () => {
+  // 준비 단계 (Arrage)
+  render(<Greeting />);
+
+  // 실행 단계 (Act)
+  // 현재는 아무 것도 없다.
+
+  // 단언 단계 (Assert)
+  const helloWorldElement = screen.getByText("Hello World!");
+  expect(helloWorldElement).toBeInTheDocument();
+});
